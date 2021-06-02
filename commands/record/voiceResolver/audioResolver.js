9f446db2dd043e84717c8b7506dca7a212b1164b
baolongv3 <baolongv4@gmail.com>
2021-06-03 02:34:18 +0700

    Fixed Async on writing to file and encoding recording

--- commands/record/voiceResolver/audioResolver.js
+++ commands/record/voiceResolver/audioResolver.js
@@ -10,14 +10,14 @@ async function convertToMp3(inputPath,outputPath){
         sfreq : 44.1,
         'little-endian' : true
     }).setFile(inputPath);
-
-    await encoder.encode().then(() => {
+    
+    encoder.encode().then(() => {
         logger.info('Encoding Finished!');
     }).catch((error) => {
         logger.error(error.stack);
     })
 }
-function appendFiles(outputStream,currentfile,inputStream,chunks) {
+async function appendFiles(outputStream,currentfile,inputStream,chunks) {
     
     if (!chunks.length) {
         outputStream.end(() => logger.info('Finished.'));
@@ -27,7 +27,7 @@ function appendFiles(outputStream,currentfile,inputStream,chunks) {
     currentfile = `${__dirname}/../../../tmp/records/` + chunks.shift();
     inputStream = fs.createReadStream(currentfile);
 
-    inputStream.pipe(outputStream, { end: false });
+    await inputStream.pipe(outputStream, { end: false });
 
     await inputStream.on('end', function() {
         logger.info(currentfile + ' appended');
@@ -39,7 +39,7 @@ function appendFiles(outputStream,currentfile,inputStream,chunks) {
 module.exports.saveRecord = async function(){
     let inputStream, currentfile, outputStream = fs.createWriteStream(__dirname + `/../../../recordings/full_${new Date().getUTCHours()}.pcm`);
     let chunks = fs.readdirSync(__dirname + '/../../../tmp/records/');
-    appendFiles(outputStream,currentfile,inputStream,chunks);
+    await appendFiles(outputStream,currentfile,inputStream,chunks);
     await convertToMp3(__dirname + `/../../../recordings/full_${new Date().getUTCHours()}.pcm`,__dirname + `/../../../recordings/full_${new Date().getUTCHours()}.mp3`,() => {
         return;
     });
