99214cf49d8f4237ffdb116be89c9650e12419b7
baolongv3 <baolongv4@gmail.com>
2021-06-03 20:07:02 +0700

    Removed Async on unnecessary function

--- commands/record/voiceResolver/audioResolver.js
+++ commands/record/voiceResolver/audioResolver.js
@@ -2,7 +2,7 @@ const Lame = require('node-lame').Lame;
 const logger = require('../../../logger');
 const fs = require('fs');
 
-async function convertToMp3(inputPath,outputPath){
+function convertToMp3(inputPath,outputPath){
     const encoder = new Lame({
         output: outputPath,
         bitrate : 192,
@@ -17,7 +17,7 @@ async function convertToMp3(inputPath,outputPath){
         logger.error(error.stack);
     })
 }
-async function appendFiles(outputStream,currentfile,inputStream,chunks) {
+function appendFiles(outputStream,currentfile,inputStream,chunks) {
     
     if (!chunks.length) {
         outputStream.end(() => logger.info('Finished.'));
@@ -27,16 +27,16 @@ async function appendFiles(outputStream,currentfile,inputStream,chunks) {
     currentfile = `${__dirname}/../../../tmp/records/` + chunks.shift();
     inputStream = fs.createReadStream(currentfile);
 
-    await inputStream.pipe(outputStream, { end: false });
+    inputStream.pipe(outputStream, { end: false });
 
-    await inputStream.on('end', function() {
+    inputStream.on('end', function() {
         logger.info(currentfile + ' appended');
         fs.unlinkSync(currentfile);
         appendFiles(outputStream,currentfile,inputStream,chunks);
     });
 }
-async function runSaveFunction(){
-    await convertToMp3(__dirname + `/../../../recordings/full_${new Date().getUTCHours()}.pcm`,__dirname + `/../../../recordings/full_${new Date().getUTCHours()}.mp3`);
+function runSaveFunction(){
+    convertToMp3(__dirname + `/../../../recordings/full_${new Date().getUTCHours()}.pcm`,__dirname + `/../../../recordings/full_${new Date().getUTCHours()}.mp3`);
 }
 module.exports.saveRecord = async function(){
     let inputStream, currentfile, outputStream = fs.createWriteStream(__dirname + `/../../../recordings/full_${new Date().getUTCHours()}.pcm`);
