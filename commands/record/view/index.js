39f62fc78f8a9e0074a91e6592a0a59c8f5561b8
baolongv3 <baolongv4@gmail.com>
2021-06-20 15:40:39 +0700

    Fixed redudant import

--- commands/record/view/index.js
+++ commands/record/view/index.js
@@ -1,17 +1,14 @@
-
-const msgEmbed = require('../../../utils/msgembed');
-
 module.exports = {
-    printInfo : function() {
+    printInfo: function () {
         return Discord.MessageEmbed()
-                .setTitle('FPTU DSC Bot')
-                .setAuthor('DSC FPT University', 'https://ca.slack-edge.com/T01SZJQ9K9P-U01U9AQ39QC-06d89ca32aeb-512')
-                .setTitle('Record')
-                .setDescription(':red_circle: Recording Utils')
-                .addField('enter','Order bot to join server')
-                .addField('start', 'Order bot to start recording')
-                .addField('stop', 'Order bot to stop recording')
-                .addField('TODO')
-        
+            .setTitle('FPTU DSC Bot')
+            .setAuthor('DSC FPT University', 'https://ca.slack-edge.com/T01SZJQ9K9P-U01U9AQ39QC-06d89ca32aeb-512')
+            .setTitle('Record')
+            .setDescription(':red_circle: Recording Utils')
+            .addField('enter', 'Order bot to join server')
+            .addField('start', 'Order bot to start recording')
+            .addField('stop', 'Order bot to stop recording')
+            .addField('TODO')
+
     }
 }
